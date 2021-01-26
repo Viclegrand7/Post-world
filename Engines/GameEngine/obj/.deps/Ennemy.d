e65f96c8d89090a16f47e05949c90726fadf05e3
zeng <zeng_ruxue@126.com>
2021-01-26 02:32:32 +0100

    mtl added

--- Engines/GameEngine/obj/.deps/Ennemy.d
+++ Engines/GameEngine/obj/.deps/Ennemy.d
@@ -1,6 +1,6 @@
 obj/Ennemy.o: src/Ennemy.cc include/Ennemy.hh include/Character.hh \
- include/Gun.hh include/Weapon.hh include/Item.hh include/Melee.hh \
- include/PowerUp.hh
+  include/Gun.hh include/Weapon.hh include/Item.hh include/Melee.hh \
+  include/PowerUp.hh
 
 include/Ennemy.hh:
 
--- Engines/GameEngine/obj/.deps/Player.d
+++ Engines/GameEngine/obj/.deps/Player.d
@@ -1,6 +1,6 @@
 obj/Player.o: src/Player.cc include/Player.hh include/Character.hh \
- include/Gun.hh include/Weapon.hh include/Item.hh include/Melee.hh \
- include/PowerUp.hh
+  include/Gun.hh include/Weapon.hh include/Item.hh include/Melee.hh \
+  include/PowerUp.hh
 
 include/Player.hh:
 
