e2207aa6f909d6934a10f3bd94e6327275308eb1
zeng <zeng_ruxue@126.com>
2021-02-05 19:16:34 +0100

    Makefile modified for testcase

--- Engines/GameEngine/tests/TestCase.cc
+++ Engines/GameEngine/tests/TestCase.cc
@@ -8,48 +8,16 @@
 #include "Melee.hh"
 #include "PowerUp.hh"
 
-TEST_CASE("1: Gun"){
-  Gun g(5, true, 3);
-  REQUIRE(g.use() == -1);
-  REQUIRE(g.getCurMag() == 20);
-  REQUIRE(g.getCurBull() == 20);
-  REQUIRE(g.attack() == true);
-  REQUIRE(g.isShooting() == true);
-  REQUIRE(g.getCurBull() == 19);
-  g.countDown();
-  REQUIRE(g.attack() == false);
-  g.countDown();
-  g.countDown();
-  REQUIRE(g.attack() == true);
-  g.addBullet(3);
-  REQUIRE(g.getCurBull() == 20);
-  REQUIRE(g.secondary() == true);
-}
 
-TEST_CASE("2: Melee"){
-  Melee M(1, 2, 0, 1);
-  REQUIRE(M.use() == -2);
-  REQUIRE(M.getRange() == 0);
-  REQUIRE(M.getDamage() == 1);
-  REQUIRE(M.attack() == true);
-  REQUIRE(M.isAuto() == false);
-  M.countDown();
-  REQUIRE(M.attack() == false);
-  M.countDown();
-  REQUIRE(M.secondary() == true);
-  M.countDown();
-  REQUIRE(M.attack() == true);
-}
-
-TEST_CASE("3: PowerUp"){
+TEST_CASE("1: PowerUp"){
   PowerUp P(1);
   REQUIRE(P.use() == 1);
 }
 
-TEST_CASE("4: Ennemy"){
+TEST_CASE("2: Ennemy"){
   Ennemy E;
   Ennemy E2(2, 5);
-  REQUIRE(E.getDamage() == 0);
+  REQUIRE(E.getDamage() == 1);
   REQUIRE(E2.getDamage() == 2);
   REQUIRE(E.getHealth() == 20);
   E.getHit(E2.getDamage());
@@ -59,7 +27,7 @@ TEST_CASE("4: Ennemy"){
   REQUIRE(E2.isDead() == true);
 }
 
-TEST_CASE("5: Player : weapons"){
+TEST_CASE("3: Player : weapons"){
   Player P1;
   REQUIRE(P1.getMaxHealth() == 20);
   Gun g1, g2;
@@ -69,9 +37,9 @@ TEST_CASE("5: Player : weapons"){
   P1.use(up0);
   REQUIRE(P1.getMaxStamina() == 15);
   P1.use(k);
-  REQUIRE(P1.getCurrentWeapon() == 0);
-  P1.use(g1);
   REQUIRE(P1.getCurrentWeapon() == 1);
+  P1.use(g1);
+  REQUIRE(P1.getCurrentWeapon() == 2);
   P1.use(g2);
   REQUIRE(P1.getCurrentWeapon() == 2);
   P1.use(k2);
@@ -84,11 +52,54 @@ TEST_CASE("5: Player : weapons"){
   REQUIRE(P1.getWeaponDamage() == 6);
   P1.nextWeapon();
   REQUIRE(P1.getCurrentWeapon() == 0);
-  REQUIRE(P1.getWeaponDamage() == 1);
   P1.nextWeapon();
   P1.dropWeapon();
   REQUIRE(P1.getCurrentWeapon() == 0);
   P1.nextWeapon();
   P1.nextWeapon();
   REQUIRE(P1.getCurrentWeapon() == 0);
+}
+
+TEST_CASE("4: Player : Gun"){
+  Player P1;
+  Gun g(5, true, 3);
+  Melee M(1, 2, 0, 1);
+  REQUIRE(g.use() == -1);
+  REQUIRE(g.getCurMag() == 20);
+  REQUIRE(g.getCurBull() == 20);
+  REQUIRE(g.isShooting() == false);
+  REQUIRE(g.attack() == false);  //isShooting = false
+  P1.use(g);
+  REQUIRE(P1.getCurrentWeapon() == 1);
+  REQUIRE(P1.attack() == false);
+  P1.toAttack();
+  REQUIRE(g.isShooting() == true);
+  REQUIRE(P1.attack() == true);
+  REQUIRE(g.getCurMag() == 19);
+  g.countDown();
+  REQUIRE(P1.attack() == false);
+  g.countDown();
+  g.countDown();
+  REQUIRE(P1.attack() == true);
+  g.addBullet(3);
+  REQUIRE(g.getCurBull() == 20);
+  REQUIRE(g.secondary() == true);
+}
+
+
+TEST_CASE("5: Player : Melee"){
+  Player P1;
+  Melee M(1, 2, 0, 1);
+  REQUIRE(M.use() == -2);
+  REQUIRE(M.getRange() == 0);
+  REQUIRE(M.getDamage() == 1);
+  REQUIRE(M.isAuto() == false);
+  P1.use(M);
+  REQUIRE(P1.getCurrentWeapon() == 1);
+  P1.toAttack();
+  REQUIRE(P1.attack() == true);
+  M.countDown();
+  REQUIRE(P1.attack() == false);
+  M.countDown();
+  REQUIRE(P1.attack() == true);
 }
